8715d3ccd5915c2dd4d5d8c1fadf570553731703
ssachethana <sandeepasachethana2@gmail.com>
2026-07-18 22:47:56 +0530

    Briefly describe your update here

--- app/api/inventory/brand/route.ts
+++ app/api/inventory/brand/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-import {prisma } from '@/lib/prisma';
 import { auth } from "@/auth"
+import {prisma } from '@/lib/prisma';
+
 
 export async function GET(request: NextRequest) {
 
